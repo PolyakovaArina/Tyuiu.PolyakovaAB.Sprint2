a75a896df1337a0338431ce29a2922c693e42c14
PolyakovaAB <Tyuiu.PolyakovaA@outlook.com>
2026-09-19 11:38:34 +0500

    Add project files.

---
+++ Tyuiu.PolyakovaAB.Sprint2.Task0.V5.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.PolyakovaAB.Sprint2.Task0.V5.Lib
+{
+    public class DataService : ISprint2Task0V5
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+            res[0] = x==y; res[1] = x != y; res[2] = x < y; res[3] = y > x; res[4] = y <= x; res[5] = y >= x; 
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.PolyakovaAB.Sprint2.Task0.V5.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.PolyakovaAB.Sprint2.Task0.V5.Lib;
+namespace Tyuiu.PolyakovaAB.Sprint2.Task0.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void Validxpression()
+        {
+            DataService ds = new DataService();
+            int x = 100;
+            int y = 200;
+            bool[] re = new bool[6];
+            re = ds.GetCompareOperations(x, y);
+            bool[] res = new bool[6] {false, true, true, true, false, true};
+            CollectionAssert.AreEqual(res,re);
+        }
+    }
+}
