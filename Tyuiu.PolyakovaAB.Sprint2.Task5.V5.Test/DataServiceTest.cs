440761d91ca0644999fa09b1aa6ebab43ee274ee
PolyakovaAB <Tyuiu.PolyakovaA@outlook.com>
2026-09-19 18:17:43 +0500

    Task5

---
+++ Tyuiu.PolyakovaAB.Sprint2.Task5.V5.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.PolyakovaAB.Sprint2.Task5.V5.Lib;
+namespace Tyuiu.PolyakovaAB.Sprint2.Task5.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpressioin()
+        {
+            DataService ds = new DataService();
+            int a = 6;
+            Assert.AreEqual("шестёрка", ds.FindCardValue(a));
+        }
+    }
+}
---
+++ Tyuiu.PolyakovaAB.Sprint2.Task5.V5/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.PolyakovaAB.Sprint2.Task5.V5.Lib;
+namespace Tyuiu.PolyakovaAB.Sprint2.Task5.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int a;
+            Console.Title = "Спринт #2 | Выполнила: Полякова А. В. | ИИПб-26-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнила: Полякова Арина Вячеславовна | ИИПб-26-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch вычисляет        *");
+            Console.WriteLine("* требуемое значение и возвращает результат.                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*Введите номер:                                                           *");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.FindCardValue(a));
+            Console.ReadKey();
+        }
+    }
+}
